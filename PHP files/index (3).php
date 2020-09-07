6f9a6145352a64464b8542954d07f1c844a9c256
chhabrap71760 <53203344+chhabrap71760@users.noreply.github.com>
2020-09-07 13:13:13 +1200

    I have completed Checkpoint 2

---
+++ PHP files/index (3).php
@@ -0,0 +1,17 @@
+  <?php include("topbit.php") ?>
+            
+        <div class="box main">
+            <h2>Welcome</h2>
+            
+            
+            <p>
+                This database includes some of the most popular apps 
+                available for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can 
+                found at the preceding link.
+            </p>
+            
+
+            
+        </div> <!-- / main -->
+        
+ <?php include("bottombit.php") ?>S
