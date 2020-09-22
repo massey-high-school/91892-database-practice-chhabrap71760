f84c16eb5c64b0a93369c1edc3bbb78596df437e
chhabrap71760 <53203344+chhabrap71760@users.noreply.github.com>
2020-09-23 10:55:54 +1200

    almost completed advanced search

--- PHP files/bottombit.php
+++ PHP files/bottombit.php
@@ -32,12 +32,54 @@
             
             <form class="searchform" method="post"
             action="advanced.php" enctype="multipart/form-data">
+            
+            <input class="adv" type="text" name="app_name" size="40"
+            value="" placeholder="App Name / Title..."/>
                 
-                <p>Form goes here...</p>
+            <input class="adv" type="text" name="dev_name" size="40"
+            value="" placeholder="Developer..."/>
                 
+            
+                
+            <!-- Genre Dropdown -->
+                
+            <select class="search adv" name="genre">
+            
+            <option value="" disabled selected>Genre...</option>
+                
+                <!-- get options from database -->    
+                <?php 
+                    $genre_sql="SELECT * FROM `L2_DB_Prac_Genre` ORDER BY `L2_DB_Prac_Genre`.`Genre` ASC";
+                    $genre_query=mysqli_query($dbconnect, $genre_sql);
+                    $genre_rs=mysqli_fetch_assoc($genre_query);
+        
+                    do {
+                        ?>
+                    <option value="<?php echo $genre_rs['Genre']; ?>"><?php
+                    echo $genre_rs['Genre']; ?></option>    
+                        
+                    <?php
+                        
+                    } // end genre do loop
+                
+                    while ($genre_rs=mysqli_fetch_assoc($genre_query))
+                
+                ?>
+                
+            </select>
+                
+                <!-- Cost -->
+                
+                <!-- No In App Checkbox -->
+                
+                <!-- Rating -->
+                
+                <!-- Age -->
+                
+            <!-- Search Button is below -->
             <input class="submit advanced-button" type="submit"
-            name="advanced" value="Search &nbsp; &#xf003;" />
-                
+            name="advanced" value="Search &nbsp; &#xf002;" />
+           
             </form>
                 
             </div> <!-- / advanced frame -->
